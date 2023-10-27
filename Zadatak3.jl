b438027f7003fe7891b05843ff64559fd8f5f230
Amina Hromic <115954313+AHromic1@users.noreply.github.com>
2023-10-27 13:04:09 +0200

    Update Zadatak3.jl

--- Zadatak3.jl
+++ Zadatak3.jl
@@ -2,7 +2,7 @@ using JuMP, GLPK
 m = Model(GLPK.Optimizer)
 @variable(m, x1>=0)
 @variable(m, x2>=0)
-@objective(m, Max, 3x1+5x2)
+@objective(m, Min, 3x1+5x2)
 @constraint(m, 2x1 + 4x2 >= 10)
 @constraint(m, 2*x1 + 6*x2 >= 10)
 
